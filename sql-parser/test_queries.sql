3831eeb5bad1d2e19370c9b22978275280e5d1a7
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-11-07 20:25:56 +0300

    add test samples

---
+++ sql-parser/test_queries.sql
@@ -0,0 +1,5 @@
+SELECT      * FROM Apps;
+select * from Apps where AppName = 'MoneyControl';
+select AppName, AppCategory from Apps;
+select distinct AppName from Apps WHERE AppName = NUll;
+select AppName from Apps where AppPrice > 60;
