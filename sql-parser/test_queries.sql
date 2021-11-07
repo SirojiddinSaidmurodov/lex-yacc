de30ac5fdab5f54085bbd93c169d6bb51f222445
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-11-07 22:02:50 +0300

    add error samples

--- sql-parser/test_queries.sql
+++ sql-parser/test_queries.sql
@@ -1,5 +1,6 @@
 SELECT      * FROM Apps;
 select * from Apps where AppName = 'MoneyControl';
 select AppName, AppCategory from Apps;
-select distinct AppName from Apps WHERE AppName = NUll;
-select AppName from Apps where AppPrice > 60;
+select distinct AppName from Apps WHERE AppName = NUll AND (Downloads > 10000);
+select AppName from 4pps where AppPrice > 60;
+select AppName from ∆pps where AppPrice > 60;
