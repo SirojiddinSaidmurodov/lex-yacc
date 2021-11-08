c9389ff01b1788cb4ec1668d648dc2062f45e665
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-11-08 22:47:18 +0300

    edit change rules

--- sql-parser/test_queries.sql
+++ sql-parser/test_queries.sql
@@ -2,5 +2,5 @@ SELECT      * FROM Apps;
 select * from Apps where AppName = 'MoneyControl';
 select AppName, AppCategory from Apps;
 select distinct AppName from Apps WHERE AppName = NUll AND (Downloads > 10000);
-select AppName from 4pps where AppPrice > 60;
+select AppName from 4pps where, AppPrice > 60;
 select AppName from ∆pps where AppPrice > 60;
