a57d307df80ad3960c1b1374961686e0a8f48d2d
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-11-09 12:17:50 +0300

    add group and order expressions

--- sql-parser/test_queries.sql
+++ sql-parser/test_queries.sql
@@ -1,6 +1,8 @@
 SELECT      * FROM Apps;
 select * from Apps where AppName = 'MoneyControl';
 select AppName, AppCategory from Apps;
-select distinct AppName from Apps WHERE AppName = NUll AND (Downloads > 10000);
+Select distinct AppName from Apps WHERE AppName = NUll AND (Downloads > 10000);
+SELECT * FROM Customers ORDER BY Country DESC;
+SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country ORDER BY COUNT(CustomerID) DESC;
 select AppName from 4pps where, AppPrice > 60;
 select AppName from ∆pps where AppPrice > 60;
